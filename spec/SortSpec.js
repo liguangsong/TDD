ce1fd91d052348019f926d1d02ab4d597787d2ad
liguangsong <863925367@qq.com>
2013-12-18 17:17:04 +0800

    猜数字完成版

--- spec/SortSpec.js
+++ spec/SortSpec.js
@@ -24,7 +24,7 @@ describe("guess the results within six", function() {
         guess_success();
         expect(document.getElementById("label").innerHTML).toBe("恭喜成功猜对");
     })
-    it ("should be 失败，答案是1234 when the three time is 1234 ",function(){
+    it ("should be 失败，答案是1234 when the last time is  not 1234 ",function(){
         var count=localStorage.getItem("count")
         guess_fail(6);
         expect(document.getElementById("label").innerHTML).toBe("失败，答案是"+count);
